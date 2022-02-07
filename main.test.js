dd3bc167fedae952d4c6a4a7a8f8ee0493de847d
mar1n <30738216+mar1n@users.noreply.github.com>
2022-02-07 23:38:48 +0000

    red test: counter history api backword forward btn

--- main.test.js
+++ main.test.js
@@ -47,68 +47,90 @@ test("persists items between sessions", () => {
   ).toBeInTheDocument();
 });
 
-describe("adding items", () => {
-
-  test("undo to empty list", () => {
-    const itemField = screen.getByPlaceholderText("Item name");
-    const submitBtn = screen.getByText("Add to inventory");
-    fireEvent.input(itemField, {
-      target: { value: "cheesecake" },
+describe("counter", () => {
+  test("undo counter ", () => {
+    const counterInput = screen.getByPlaceholderText("input-counter");
+    const buttonCounter = screen.getByPlaceholderText("button-plus");
+    fireEvent.input(counterInput, {
+      target: { value: 1 },
       bubbles: true,
     });
 
-    const quantityField = screen.getByPlaceholderText("Quantity");
-    fireEvent.input(quantityField, {
-      target: { value: "6" },
-      bubbles: true,
-    });
+    fireEvent.click(buttonCounter);
 
-    fireEvent.click(submitBtn);
-
-    expect(history.state).toEqual({ inventory: { cheesecake: 6 } });
+    expect(history.state).toEqual({ counter: 1});
     window.addEventListener("popstate", () => {
-      const itemList = document.getElementById("item-list");
-      expect(itemList).toBeEmptyDOMElement();
+      const counterField = document.getElementById("counter-view");
+      expect(counterField).toBeEmptyDOMElement();
       done();
     });
-    fireEvent.click(screen.getByText("Undo"));
-  });
 
-  test("undo to one item", (done) => {
-    const itemField = screen.getByPlaceholderText("Item name");
-    const quantityField = screen.getByPlaceholderText("Quantity");
-    const submitBtn = screen.getByText("Add to inventory");
-
-    fireEvent.input(itemField, {
-      target: { value: "cheesecake" },
-      bubbles: true,
-    });
-    fireEvent.input(quantityField, {
-      target: { value: "6" },
-      bubbles: true,
-    });
-    fireEvent.click(submitBtn);
-
-    fireEvent.input(itemField, {
-      target: { value: "carrot cake" },
-      bubbles: true,
-    });
-    fireEvent.input(quantityField, {
-      target: { value: "5" },
-      bubbles: true,
-    });
-    fireEvent.click(submitBtn);
-    window.addEventListener("popstate", () => {
-      const itemList = document.getElementById("item-list");
-      expect(itemList.children).toHaveLength(1);
-      expect(
-        getByText(itemList, "cheesecake - Quantity: 6")
-      ).toBeInTheDocument();
-      done();
-    });
     fireEvent.click(screen.getByText("Undo"));
   });
 });
+
+// describe("adding items", () => {
+
+//   test("undo to empty list", () => {
+//     const itemField = screen.getByPlaceholderText("Item name");
+//     const submitBtn = screen.getByText("Add to inventory");
+//     fireEvent.input(itemField, {
+//       target: { value: "cheesecake" },
+//       bubbles: true,
+//     });
+
+//     const quantityField = screen.getByPlaceholderText("Quantity");
+//     fireEvent.input(quantityField, {
+//       target: { value: "6" },
+//       bubbles: true,
+//     });
+
+//     fireEvent.click(submitBtn);
+
+//     expect(history.state).toEqual({ inventory: { cheesecake: 6 } });
+//     window.addEventListener("popstate", () => {
+//       const itemList = document.getElementById("item-list");
+//       expect(itemList).toBeEmptyDOMElement();
+//       done();
+//     });
+//     fireEvent.click(screen.getByText("Undo"));
+//   });
+
+//   test("undo to one item", (done) => {
+//     const itemField = screen.getByPlaceholderText("Item name");
+//     const quantityField = screen.getByPlaceholderText("Quantity");
+//     const submitBtn = screen.getByText("Add to inventory");
+
+//     fireEvent.input(itemField, {
+//       target: { value: "cheesecake" },
+//       bubbles: true,
+//     });
+//     fireEvent.input(quantityField, {
+//       target: { value: "6" },
+//       bubbles: true,
+//     });
+//     fireEvent.click(submitBtn);
+
+//     fireEvent.input(itemField, {
+//       target: { value: "carrot cake" },
+//       bubbles: true,
+//     });
+//     fireEvent.input(quantityField, {
+//       target: { value: "5" },
+//       bubbles: true,
+//     });
+//     fireEvent.click(submitBtn);
+//     window.addEventListener("popstate", () => {
+//       const itemList = document.getElementById("item-list");
+//       expect(itemList.children).toHaveLength(1);
+//       expect(
+//         getByText(itemList, "cheesecake - Quantity: 6")
+//       ).toBeInTheDocument();
+//       done();
+//     });
+//     fireEvent.click(screen.getByText("Undo"));
+//   });
+// });
 
 test("adding items through the form", () => {
   screen.getByPlaceholderText("Item name").value = "cheesecake";
